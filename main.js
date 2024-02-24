8b1ece96480d5646b4678207543c93b1ef2c49d8
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2024-02-24 20:11:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-let gameOver = confirm('Do You want to play the game then Enter!')
-if(gameOver == true ){
-    let playerChoice = prompt('Enter your choice from rock,paper or scissors').toLowerCase()
-    if(playerChoice =='rock' || playerChoice =='paper' || playerChoice =='scissors'){
-        let computerChoice = Math.floor(Math.random()*3 + 1)
-        let computer = computerChoice == 1 ? 'rock' : computerChoice == 2 ? 'paper' : 'scissors';
-        console.log(computer)
-        if((playerChoice == 'rock' && computer == 'scissors')||(playerChoice == 'paper' && computer == 'rock')||(playerChoice == 'scissors' && computer == 'paper')){
-            alert('You Win')
-        }else{
-            alert('You Lose')
-        }
-    }else{
-        alert('Please Enter the correct input ')
-    }
-}else {
-    alert('Game Over please try next time !')
+const colors = ["orange" , "green" , "yellow", "purple" , "blue"]
+
+const randomIndex = Math.floor(Math.random() * colors.length)
+
+// console.log(randomIndex)
+
+const randomColors = colors[randomIndex]
+
+// console.log(randomColors)
+
+switch (randomColors) {
+    case "orange" :
+        console.log("This is Orange color")
+        break
+    case "green" :
+        console.log("This is green color ")
+        break
+    case "yellow" :
+        console.log("This is yellow color")
+        break
+    default :
+        console.log("No Color is Found")
 }
--- main2.js
+++ main2.js
@@ -1,32 +1,15 @@
-// let myNumber = 0;
-while(myNumber <= 50){
-//     console.log(myNumber)
-//     myNumber++
+// for (let index = 0 ; index < 10 ; index=index+1){
+//     console.log(index)
+// }
+
+function myFunction(){
+    console.log("Hello World , This is my javscript function ")
 }
 
-for (let myNumber=0;myNumber<=50;myNumber+=2){
-    // console.log(myNumber)
+// arraow function 
+
+const arraowFunction = ()=> {
+    console.log("I am a arraow function")
 }
 
-let name = 'Devender'
-for (let i=0;i<name.length;i++){
-    // console.log(name.charAt(i))
-}
-
-function sum(num1 ,num2){
-    if(num2 == undefined){
-        return num1+num1
-    }
-    return num1+num2
-}
-// console.log(sum(2))
-
-const getUserEmail = (email)=>{
-    return email.slice(0,email.indexOf('@'))
-}
-// console.log(getUserEmail('playerTwo@gmail.com'))
-
-const toProperCase = (name)=>{
-   return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-}
-// console.log(toProperCase('deveNDER'))
+arraowFunction()
